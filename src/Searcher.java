d35b2604490c41741f3b0e7b05e72d3d7daa0b46
Julian Osmer <julian.osmer@outlook.de>
2021-02-17 17:43:29 +0100

    update

--- src/Searcher.java
+++ src/Searcher.java
@@ -8,7 +8,7 @@ public class Searcher {
 
   private static final int[] lastOcc = new int[256];
   private static final Map<Integer, String> LCP = new HashMap<>();
-  private static final List<Integer> results = new ArrayList<>();
+  private static List<Integer> indices = new ArrayList<>();
   private static List<String> L = new ArrayList<>();
   private static String pat = null;
   private static String text = null;
@@ -29,27 +29,25 @@ public class Searcher {
 
     for (int measurement1 = 0; measurement1 < runs; measurement1++) {
       searchStartTime = System.nanoTime();
-      naiveSlidingWindow(false);
+      indices = naiveSlidingWindow(false);
       searchRuntime += (System.nanoTime() - searchStartTime);
     }
 
-    System.out.println("Sliding Window = " + results);
+    System.out.println("Sliding Window = " + indices);
     System.out.println("Search runtime (Nano): " + (searchRuntime / runs));
     System.out.println();
 
-    results.clear();
     searchRuntime = 0;
     for (int measurement2 = 0; measurement2 < runs; measurement2++) {
       searchStartTime = System.nanoTime();
-      naiveSlidingWindow(true);
+      indices = naiveSlidingWindow(true);
       searchRuntime += (System.nanoTime() - searchStartTime);
     }
 
-    System.out.println("Sliding Window with last-occ = " + results);
+    System.out.println("Sliding Window with last-occ = " + indices);
     System.out.println("Search runtime (Nano): " + (searchRuntime / runs));
     System.out.println();
 
-    results.clear();
     searchRuntime = 0;
     for (int measurement3 = 0; measurement3 < runs; measurement3++) {
       text = readText(args[1]);
@@ -57,16 +55,17 @@ public class Searcher {
       preprocessingStartTime = searchStartTime;
       offlineSearchPreprocessing();
       preprocessingRuntime += (System.nanoTime() - preprocessingStartTime);
-      simpleSearch();
+      indices = simpleSearch();
       searchRuntime += (System.nanoTime() - searchStartTime);
     }
 
-    System.out.println("Simple Search = " + results);
+    System.out.println("Simple Search = " + indices);
     System.out.println("Preprocessing runtime (Nano): " + (preprocessingRuntime / runs));
     System.out.println("Search runtime (Nano): " + ((searchRuntime / runs) - (preprocessingRuntime / runs)));
   }
 
-  public static boolean naiveSlidingWindow(boolean intelligent) {
+  public static List<Integer> naiveSlidingWindow(boolean intelligent) {
+    List<Integer> indices = new ArrayList<>();
     if (intelligent) {
       initLastOcc(pat);
     }
@@ -79,8 +78,7 @@ public class Searcher {
         j++;
       }
       if (j == m) {
-        results.add(i);
-        return true;
+        indices.add(i);
       }
       if (intelligent) {
         i += lastOcc[text.charAt(i + m - 1)];
@@ -88,10 +86,11 @@ public class Searcher {
         i++;
       }
     }
-    return false;
+    return indices;
   }
 
-  public static int simpleSearch() {
+  public static List<Integer> simpleSearch() {
+    List<Integer> indices = new ArrayList<>();
     int d = -1;
     int f = L.size();
     final int m = pat.length();
@@ -99,15 +98,15 @@ public class Searcher {
       int i = (d + f) / 2;
       final int l = LCP.get(i).length();
       if (l == m && l == L.get(i).length()) {
-        results.add(i);
-        return i;
+        indices.add(i);
+        f = i;
       } else if (l == L.get(i).length() || (l != m && L.get(i).charAt(l) < pat.charAt(l))) {
         d = i;
       } else {
         f = i;
       }
     }
-    return -1;
+    return indices;
   }
 
   public static void offlineSearchPreprocessing() {
